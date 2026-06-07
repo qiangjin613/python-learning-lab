64435865507a297d9082ce34fc26230203a0828c
gainie <gainie@dtstack.com>
2026-06-07 17:18:28 +0800

    lcp72

--- 03-algorithm/leetcode/LCP_72_hqCnmP.py
+++ 03-algorithm/leetcode/LCP_72_hqCnmP.py
@@ -1,5 +1,15 @@
 #https://leetcode.cn/problems/hqCnmP/
 
+from typing import List
+
 class Solution:
     def supplyWagon(self, supplies: List[int]) -> List[int]:
-        
+        m = len(supplies) // 2
+        while len(supplies) > m:
+            idx = 1
+            for i in range(1, len(supplies)):
+                if supplies[i - 1] + supplies[i] < supplies[idx - 1] + supplies[idx]:
+                    idx = i
+            supplies[idx - 1] += supplies[idx]
+            supplies.pop(idx)
+        return supplies
